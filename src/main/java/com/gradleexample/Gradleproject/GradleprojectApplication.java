68dfd8827385191073cc2da60374d439b375922e
guptaneha346 <neha.gupta@impressico.com>
2020-02-19 12:42:17 +0530

    First commit

---
+++ src/main/java/com/gradleexample/Gradleproject/GradleprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gradleexample.Gradleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gradleexample/Gradleproject/GradleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradleexample.Gradleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
